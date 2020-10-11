ff317dea4e1de45bf8e11f63bbf81270efdcb8d0
Christopher Iliyas <christopheriliyas@gmail.com>
2020-10-11 23:56:43 +0100

    finish video chat app

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express, { Application } from 'express';
 import socketIO, { Server as SocketIOServer } from 'socket.io';
 import { createServer, Server as HttpServer } from 'http';
@@ -7,18 +8,21 @@ export class Server {
   private app: Application;
   private io: SocketIOServer;
 
+  private activeSockets: string[] = [];
   private readonly PORT = 8000;
 
   constructor() {
     this.initialize();
-    this.handleRoutes();
-    this.handleSocketConnection();
   }
 
   private initialize(): void {
     this.app = express();
     this.httpServer = createServer(this.app);
     this.io = socketIO(this.httpServer);
+
+    this.configureApp();
+    this.handleRoutes();
+    this.handleSocketConnection();
   }
   
   private handleRoutes(): void {
@@ -29,8 +33,57 @@ export class Server {
   
   private handleSocketConnection(): void {
     this.io.on("connection", socket => {
-      console.log("Socket connected.");
+      const existingSocket = this.activeSockets.find(
+        existingSocket => existingSocket === socket.id
+      );
+
+      if (!existingSocket) {
+        this.activeSockets.push(socket.id);
+
+        socket.emit("update-user-list", {
+          users: this.activeSockets.filter(
+            existingSocket => existingSocket !== socket.id
+          )
+        });
+
+        socket.broadcast.emit("update-user-list", {
+          users: [socket.id]
+        });
+      }
+
+      socket.on("call-user", (data: any) => {
+        socket.to(data.to).emit("call-made", {
+          offer: data.offer,
+          socket: socket.id
+        });
+      });
+
+      socket.on("make-answer", data => {
+        socket.to(data.to).emit("answer-made", {
+          socket: socket.id,
+          answer: data.answer
+        });
+      });
+
+      socket.on("reject-call", data => {
+        socket.to(data.from).emit("call-rejected", {
+          socket: socket.id
+        });
+      });
+
+      socket.on("disconnect", () => {
+        this.activeSockets = this.activeSockets.filter(
+          existingSocket => existingSocket !== socket.id
+        );
+        socket.broadcast.emit("remove-user", {
+          socketId: socket.id
+        });
+      });
     });
+  }
+
+  private configureApp(): void {
+    this.app.use(express.static(path.join(__dirname, "../public")));
   }
   
   public listen(callback: (port: number) => void): void {
